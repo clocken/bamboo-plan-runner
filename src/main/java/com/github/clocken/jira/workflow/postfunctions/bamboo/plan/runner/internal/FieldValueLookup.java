d594186d11604cb4c659947c50be03531f4ff77f
clocken <66382986+clocken@users.noreply.github.com>
2020-07-04 14:59:41 +0200

    Improve field <--> value substitution
     * support custom field names, too
     * add comments
     * some code readability improvement

--- src/main/java/com/github/clocken/jira/workflow/postfunctions/bamboo/plan/runner/internal/FieldValueLookup.java
+++ src/main/java/com/github/clocken/jira/workflow/postfunctions/bamboo/plan/runner/internal/FieldValueLookup.java
@@ -27,44 +27,93 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * This class finds JIRA field values from an issue by <i>System field ID</i>, <i>Custom field ID</i> or
+ * <i>Custom field name</i>. Ambiguous custom field names get rejected.
+ */
 public class FieldValueLookup extends StrLookup {
 
     private static final Logger LOG = LoggerFactory.getLogger(FieldValueLookup.class);
 
-    private final Issue issue;
     private final I18nHelper i18nHelper;
+    private final Issue issue;
 
+    /**
+     * Constructs a new {@link FieldValueLookup} instance.
+     *
+     * @param i18nHelper The I18n helper service for localization of the error message for unresolvable fields
+     * @param issue      The issue to lookup field values from
+     */
     public FieldValueLookup(I18nHelper i18nHelper,
                             Issue issue) {
         this.i18nHelper = i18nHelper;
         this.issue = issue;
     }
 
+    /**
+     * Looks up a <i>System field ID</i>, <i>Custom field ID</i> or <i>Custom field name</i> to the corresponding value
+     * in the issue given at construction time.
+     *
+     * @param key The System field ID, Custom field ID or Custom field name to lookup
+     * @return The value of the field in the issue, if found, an empty string if the value is <code>null</code> or a
+     * localized error text, if the field is not found
+     * @throws IllegalArgumentException if <code>key</code> is an ambiguous custom field name
+     */
     @Override
     public String lookup(String key) {
+        return customFieldValue(key)
+                .orElseGet(() -> {
+                    LOG.warn("Custom field '{}' not resolved. Trying as system field.", key);
+                    return systemFieldValue(key)
+                            .orElseGet(() -> {
+                                LOG.error("Field '{}' is not resolvable.", key);
+                                return i18nHelper.getText("bamboo-plan-runner.postfunction.field.value.unresolved");
+                            });
+                });
+    }
+
+    private Optional<String> customFieldValue(String key) {
         CustomField customField = ComponentAccessor.getCustomFieldManager()
                 .getCustomFieldObject(key);
-        if (customField != null) {
-            if (customField.getValueFromIssue(issue) == null) {
-                LOG.warn("Field '{}' resolved to null. Returning empty string.", key);
-                return StringUtils.EMPTY;
+        if (customField == null) {
+            Collection<CustomField> customFieldObjects = ComponentAccessor.getCustomFieldManager()
+                    .getCustomFieldObjectsByName(key);
+            if (customFieldObjects.isEmpty()) {
+                LOG.warn("Custom field '{}' not found.", key);
+                return Optional.empty();
+            } else if (customFieldObjects.size() > 1) {
+                throw new IllegalArgumentException(
+                        MessageFormat.format("Ambiguous custom field name {0}. Use the custom field ID instead.", key));
             }
-
-            return customField.getValueFromIssue(issue);
+            customField = customFieldObjects
+                    .iterator()
+                    .next();
         }
 
+        if (customField.getValueFromIssue(issue) == null) {
+            LOG.warn("Value of custom field '{}' resolved to null. Returning empty string.", key);
+            return Optional.of(StringUtils.EMPTY);
+        }
+
+        return Optional.of(customField.getValueFromIssue(issue));
+    }
+
+    private Optional<String> systemFieldValue(String key) {
         Field field = ComponentAccessor.getFieldManager()
                 .getField(key);
         if (field == null) {
-            LOG.error("Field '{}' not found.", key);
-            return i18nHelper.getText("bamboo-plan-runner.postfunction.field.value.unresolved");
+            LOG.warn("System field '{}' not found.", key);
+            return Optional.empty();
         }
         if (!(field instanceof ExportableSystemField)) {
-            LOG.error("Value of field '{}' is not exportable.", key);
-            return i18nHelper.getText("bamboo-plan-runner.postfunction.field.value.unresolved");
+            LOG.warn("Value of system field '{}' is not exportable.", key);
+            return Optional.empty();
         }
 
         final List<String> fieldValues = new ArrayList<>();
@@ -74,6 +123,6 @@ public class FieldValueLookup extends StrLookup {
                 .ifPresent(fieldExportPart ->
                         fieldExportPart.getValues().forEach(fieldValues::add));
 
-        return StringUtils.join(fieldValues, ", ");
+        return Optional.of(StringUtils.join(fieldValues, ", "));
     }
 }
